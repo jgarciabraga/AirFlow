600e9c6b16d2b65ea075bcdbfe52461b05156e83
jgarciabraga <jgarciabraga@gmail.com>
2023-05-29 20:39:26 -0300

    simple dags and complex dags

---
+++ trigger_dags/firsttriggerdag.py
@@ -0,0 +1,12 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime
+
+with DAG('first_trigger_dag', description="First DAG with trigger rule", schedule_interval=None, start_date=datetime(2023,1,1), catchup=False) as dag:
+    
+    task1 = BashOperator(task_id="tsk1", bash_command="sleep 5")
+    task2 = BashOperator(task_id="tsk2", bash_command="sleep 5")
+    task3 = BashOperator(task_id="tsk3", bash_command="sleep 5", trigger_rule="one_failed")
+
+    [task1, task2] >> task3
+    
